6621f7dfd2888b7b93a67eab36de3903ed332d9e
Florian Eiling <florian.eiling@ipa.fraunhofer.de>
2021-03-02 08:12:15 +0100

    Implement simple DummyHybrid Environment
    - Takes a list [int, int, int] of parameter dimensions
    - Observations are normal samples
    - Goal is to give parameters that sum to 0
    - Environments ends if n_steps >= 50 or if the sum is close to zero

--- hmlf/environments/__init__.py
+++ hmlf/environments/__init__.py
@@ -1,5 +1,6 @@
 from hmlf.environments.bipedal_walker import PaAcBipedalWalker
 from hmlf.environments.dummy_env import DummyEnv
+from hmlf.environments.dummy_hybrid import DummyHybrid
 from hmlf.environments.obstacle_course import ObstacleCourse
 from hmlf.environments.obstacle_course_v2 import ObstacleCourse_v2
 from hmlf.environments.wrapper import OneHotWrapper, SequenceWrapper, SimpleHybridWrapper
