428bd965a898ae91eb76f776da37d87a8d3c1c3c
Florian Eiling <florian.eiling@ipa.fraunhofer.de>
2021-03-05 15:26:42 +0100

    Use new HybridBase class
    - Removed type checking in preprocessing.py
    - Some type checks in policys.py/predict can probably still be removed

--- hmlf/spaces/__init__.py
+++ hmlf/spaces/__init__.py
@@ -1,4 +1,5 @@
 from hmlf.spaces.continuous_parameters import ContinuousParameters
 from hmlf.spaces.gym import Box, Dict, Discrete, MultiBinary, MultiDiscrete, Space, Tuple, utils
+from hmlf.spaces.hybrid_base import HybridBase
 from hmlf.spaces.onehot_hybrid import OneHotHybrid
 from hmlf.spaces.simple_hybrid import SimpleHybrid
