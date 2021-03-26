33488357b9d4fe5bced22e60c6c71718443e2635
Florian Eiling <florian.eiling@ipa.fraunhofer.de>
2021-03-26 13:32:31 +0100

    Rename file make_environment to wrap_environment

--- hmlf/environments/__init__.py
+++ hmlf/environments/__init__.py
@@ -1,7 +1,7 @@
 from hmlf.environments.bipedal_walker import PaAcBipedalWalker
 from hmlf.environments.dummy_env import DummyEnv
 from hmlf.environments.dummy_hybrid import DummyHybrid
-from hmlf.environments.make_environment import wrap_environment
 from hmlf.environments.obstacle_course import ObstacleCourse
 from hmlf.environments.obstacle_course_v2 import ObstacleCourse_v2
+from hmlf.environments.wrap_environment import wrap_environment
 from hmlf.environments.wrapper import OneHotWrapper, SequenceWrapper, SimpleHybridWrapper
